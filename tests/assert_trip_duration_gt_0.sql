f53c4ebd62d1204a8eab036ce0f0925025b922c3
Malik <78315034+Tismalik@users.noreply.github.com>
2024-03-07 02:19:25 +0000

    Delete tests directory

--- tests/assert_trip_duration_gt_0.sql
+++
@@ -1,4 +0,0 @@
-select * 
-from {{ ref('taxi_rides_raw') }}
--- Remember when setting up test reverse the logic to return only results that fail the test
-where tpep_dropoff_datetime = tpep_pickup_datetime
--- tests/generic/check_gt_0.sql
+++
@@ -1,5 +0,0 @@
-{% test check_gt_0(model, column_name) %}
-select * 
-from {{ model }}
-where {{ column_name }} <= 0
-{% endtest %}
