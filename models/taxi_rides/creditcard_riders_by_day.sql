e2611cfd0ccd7aaae747a8ef88d5e12ee2eb9353
Malik <78315034+Tismalik@users.noreply.github.com>
2024-03-07 02:11:35 +0000

    Delete models/taxi_rides directory

--- models/taxi_rides/creditcard_riders_by_day.sql
+++
@@ -1,7 +0,0 @@
--- Update SQL to use Jinja reference (includes task2)
-select 
-   date_part('day', tpep_pickup_datetime) as day,
-   count(*) as total_riders
-from {{ ref('taxi_rides_raw') }}
-where payment_type = 1
-group by day
