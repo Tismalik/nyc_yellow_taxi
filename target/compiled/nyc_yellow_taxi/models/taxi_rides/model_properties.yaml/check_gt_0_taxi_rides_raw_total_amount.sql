42f19d18a9d861e0977295024c7a66cd63c87583
Malik <78315034+Tismalik@users.noreply.github.com>
2024-03-07 02:19:49 +0000

    Delete target directory

--- target/compiled/nyc_yellow_taxi/models/taxi_rides/model_properties.yaml/check_gt_0_taxi_rides_raw_total_amount.sql
+++
@@ -1,4 +0,0 @@
-
-select * 
-from "dbt"."main"."taxi_rides_raw"
-where total_amount <= 0
--- target/compiled/nyc_yellow_taxi/models/taxi_rides/model_properties.yaml/columns_equal_taxi_rides_raw_8a4c18eecaabffcffe7e044ddfa588a6.sql
+++
@@ -1,4 +0,0 @@
-
-select *
-from "dbt"."main"."taxi_rides_raw"
-where tpep_pickup_datetime = tpep_dropoff_datetime
