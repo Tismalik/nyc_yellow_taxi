b78db5d33406678909f33622e2471b04d9bca42d
Malik <78315034+Tismalik@users.noreply.github.com>
2024-03-08 15:51:41 +0000

    Delete models/taxi_data directory

--- models/taxi_data/model2.sql
+++
@@ -1,8 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select * country_code,zipcode,place,state,state_code,province,province_code,latitude,longitude
-from 
-    (select *, ROW_NUMBER() over(partition by province_code order by zipcode) as rn 
-    from {{ ref('zipcodes') }}) as tb 
-where state = 'New York' and rn = 1
--- models/taxi_data/model3.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized='view') }}
-select A.* , B.*
-from {{ ref('model1') }} as A innerjoin {{ ref('model2') }} as B on  A.DOLocationID = B.province_code
-
