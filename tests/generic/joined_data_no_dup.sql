b99fa00c945395d64d60d7f18b7033531382b7f7
Malik <78315034+Tismalik@users.noreply.github.com>
2024-03-08 15:52:21 +0000

    Delete tests directory

--- tests/generic/joined_data_no_dup.sql
+++
@@ -1,5 +0,0 @@
-{% joined_data_no_dups(model, column_name, column_name2) %}
-select *
-from {{ model }}
-where {{ column_name }} <> {{ column_name2 }}
-{% endtest %}
--- tests/seed_zipcode_unique.sql
+++
@@ -1,5 +0,0 @@
---if this test returns true then if means our model is faulty so expect no 0 tip_amounts
-select count(province_code) 
-from {{ ref('model2') }}
-group by province_code
-having count(province_code) > 1
